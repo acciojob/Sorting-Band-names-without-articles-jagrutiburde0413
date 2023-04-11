54197e790aa3de64c6c553c72f7f2dda732dac2d
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-11 11:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+let toursitSpot=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+var compare=function(a,b){
+	var titleA=a.toLowerCase();
+	var titleB=b.toLowerCase();
+
+	titleA=removeArticles(titleA)
+	titleB=removeArticles(titleB)
+
+	if(titleA>titleB){
+		return 1;
+	}if(titleA<titleB){
+		return -1;
+	}
+	
+}
+function removeArticles(str){
+   let	words=str.split(" ")
+	if(word.length<=1){
+		return str;
+	}
+	if(words[0]=='a' ||words[0]=='an' ||words[0]=='the' ){
+		return words.splice(1).join(" ")	
+	}return str;
+}
+var sorted=toursitSpot.sort(compare);
+console.log(sorted)
